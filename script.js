e53148fbf420f5a85e1496b1929f2e20e8735db5
Rhisav Ray <71272108+RhisavRay@users.noreply.github.com>
2023-08-22 14:58:55 +0530

    All elements assigned to variables

---
+++ script.js
@@ -0,0 +1,10 @@
+let container = document.querySelector(".container");
+let gridBtn = document.getElementById(".submit-grid");
+let clearGridBtn = document.getElementById(".clear-grid");
+let gridWidth = document.getElementById(".width-range");
+let gridLength = document.getElementById(".height-range");
+let colorBtn = document.getElementById(".color-input");
+let eraseBtn = document.getElementById(".paint-btn");
+let paintBtn = document.getElementById(".paint-btn");
+let widthValue = document. getElementById(".height-value");
+let heightValue = document.getElementById(".height-value");
